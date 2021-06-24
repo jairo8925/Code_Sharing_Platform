f88459797a7d1b080baacd801b1468b22fef9151
Christian Jairo Sarmiento <55115040+jairo8925@users.noreply.github.com>
2021-06-23 23:38:57 -0700

    added scripts.js and styles.css in separate static folder

--- Code Sharing Platform/task/src/platform/Code.java
+++ Code Sharing Platform/task/src/platform/Code.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 
 public class Code {
 
-    private static final String PATTERN = "yyyy-MM-dd HH:mm:ss.SSS";
+    private static final String PATTERN = "yyyy-MM-dd HH:mm:ss";
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(PATTERN);
     public static final String STARTER_CODE = "public static void main(String[] args) {\n" +
             "    SpringApplication.run(CodeSharingPlatform.class, args);\n" +
--- Code Sharing Platform/task/src/platform/Controller.java
+++ Code Sharing Platform/task/src/platform/Controller.java
@@ -4,36 +4,35 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class Controller {
 
-    private static final Stack<Code> codeList = new Stack<>();
+    private static final Stack<Code> codeSnippets = new Stack<>();
     private static final AtomicInteger counter = new AtomicInteger();
 
-    @GetMapping(value = "/code/{N}", produces = "text/html")
-    public ModelAndView getNthCodeAsHtml(@PathVariable(value = "N") int n) {
+    @GetMapping(value = "/code/{index}", produces = "text/html")
+    public ModelAndView getNthCodeAsHtml(@PathVariable(value = "index") int index) {
         ModelAndView model = new ModelAndView("index");
-        Code code = codeList.get(n - 1);
+        Code code = codeSnippets.get(index - 1);
         model.addObject("code", code.getCode());
         model.addObject("date", code.getDate());
         return model;
     }
 
-    @GetMapping(value = "/api/code/{N}", produces = "application/json")
-    public Code getNthCodeAsJson(@PathVariable(value = "N") int n) {
-        return codeList.get(n - 1);
+    @GetMapping(value = "/api/code/{index}", produces = "application/json")
+    public Code getNthCodeAsJson(@PathVariable(value = "index") int index) {
+        return codeSnippets.get(index - 1);
     }
 
     @GetMapping(value = "/code/latest", produces = "text/html")
     public ModelAndView getLatestCodeSnippetsAsHtml() {
         ModelAndView model = new ModelAndView("latest");
         List<Code> latestCodeSnippets = new ArrayList<>();
-        int size = Math.min(codeList.size(), 10);
+        int size = Math.min(codeSnippets.size(), 10);
         for (int i = 0; i < size; i++) {
-            latestCodeSnippets.add(codeList.get(codeList.size() - 1 - i));
+            latestCodeSnippets.add(codeSnippets.get(codeSnippets.size() - 1 - i));
         }
         model.addObject("snippets", latestCodeSnippets);
         return model;
@@ -42,11 +41,9 @@ public class Controller {
     @GetMapping(value = "/api/code/latest", produces = "application/json")
     public List<Code> getLatestCodeSnippetsAsJson() {
         List<Code> latestCodeSnippets = new ArrayList<>();
-        int size = Math.min(codeList.size(), 10);
-        //System.out.println(codeList);
+        int size = Math.min(codeSnippets.size(), 10);
         for (int i = 0; i < size; i++) {
-            latestCodeSnippets.add(codeList.get(codeList.size() - 1 - i));
-            System.out.println(codeList.get(codeList.size() - 1 - i));
+            latestCodeSnippets.add(codeSnippets.get(codeSnippets.size() - 1 - i));
         }
         return latestCodeSnippets;
     }
@@ -59,7 +56,7 @@ public class Controller {
     @PostMapping(value = "/api/code/new", produces = "application/json")
     public Map<String, ?> updateCode(@RequestBody Code code) {
         Code newCode = new Code(code.getCode(), counter.incrementAndGet());
-        codeList.add(newCode);
+        codeSnippets.add(newCode);
         return Map.of(
                 "id", counter.toString()
         );
